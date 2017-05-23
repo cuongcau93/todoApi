bb34ce2908bc8ed9bde2ea76b550dad201b7b942
cuongcau93 <cuongnm22@wru.vn>
2017-05-23 16:26:34 +0700

    ver1
    ver1

---
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Key { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public bool isFullTime { get; set; }
+        public string paymentType { get; set; }
+        public string primaryLanguage { get; set; }
+        public string dateHired { get; set; }
+    }
+}
